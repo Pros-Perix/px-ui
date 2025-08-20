11161544a9cfdaa98ce4204c2316b0797931e5f1
Irshath <irshathv2@gmail.com>
2025-08-20 17:39:02 +0530

    add tooltip

--- packages/core/src/utils/classnames.ts
+++ packages/core/src/utils/classnames.ts
@@ -1,6 +1,28 @@
 import classnames, { ArgumentArray } from "classnames";
-import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
+
+// Create a custom tailwind-merge instance that knows about our custom utilities
+const customTwMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      "font-size": [
+        // Add our custom text-ppx-* utilities to the font-size group instead of text-color
+        {
+          text: [
+            "ppx-h1",
+            "ppx-h2",
+            "ppx-h3",
+            "ppx-h4",
+            "ppx-base",
+            "ppx-sm",
+            "ppx-xs",
+          ],
+        },
+      ],
+    },
+  },
+});
 
 export function cn(...inputs: ArgumentArray) {
-  return twMerge(classnames(inputs));
+  return customTwMerge(classnames(inputs));
 }
