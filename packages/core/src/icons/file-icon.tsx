d1cbee64295c3f64f2cc7c78fde0e9816a67d047
mokshmodi <mokshmodi96@gmail.com>
2025-12-11 10:45:19 +0530

    remove duplicate icons

---
+++ packages/core/src/icons/file-icon.tsx
@@ -0,0 +1,20 @@
+import { cn } from "../utils";
+
+export default function FileIcon(props: React.ComponentProps<"svg">) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+      className={cn("shrink-0", props.className)}
+    >
+      <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
+      <polyline points="14,2 14,8 20,8" />
+    </svg>
+  );
+}
