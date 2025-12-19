4f2f8766a98f3ab9960f3fc823aed7968618a1b5
mokshmodi <mokshmodi96@gmail.com>
2025-12-19 12:47:10 +0530

    feat: add country select field component exports

--- packages/forms/src/index.ts
+++ packages/forms/src/index.ts
@@ -5,6 +5,11 @@ export {
   type Currency,
   type CurrencySelectFieldProps,
 } from "./components/currency-select-field";
+export {
+  CountrySelectField,
+  type Country,
+  type CountrySelectFieldProps,
+} from "./components/country-select-field";
 export { FileUploadField } from "./components/file-upload-field";
 export * as Field from "./components/field";
 export { FormInput, FormTextarea, FormCheckbox } from "./components/form";
