365ec2c4ccc6349de7ad50c5b7ea90a5953ce25c
anit <anitrai011@gmail.com>
2026-01-21 02:44:06 +0530

    add tailwind source setup for ai package

--- packages/ai/src/index.ts
+++ packages/ai/src/index.ts
@@ -1,3 +1,6 @@
+// This tells the bundler to include the CSS file on the build output
+import "./index.css";
+
 // Components
 export { Xandi, type XandiProps, type Message } from "./components/xandi";
 export { type Suggestion } from "./components/x-main-intake";
