1ac0074fec5bba820a708e08fe8ec8ce128fb7f4
anit <anitrai011@gmail.com>
2026-01-21 15:53:15 +0530

    add message action icons

---
+++ packages/ai/src/assets/icons/check-icon.tsx
@@ -0,0 +1,18 @@
+export function CheckIcon(props: React.ComponentProps<"svg">) {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M20 6 9 17l-5-5" />
+    </svg>
+  );
+}
+
