87944a0f13e7c12dd24eb503d8cbf75717dfbf73
Irshath <irshathv2@gmail.com>
2026-02-11 15:18:01 +0530

    update search on popup example

--- apps/docs/src/components/combobox-with-search-demo.tsx
+++ apps/docs/src/components/combobox-with-search-demo.tsx
@@ -18,18 +18,24 @@ export function ComboboxWithSearchDemo() {
   const [value, setValue] = useState<User | null>(null);
 
   return (
-    <Combobox.Root items={users} value={value} onValueChange={setValue}>
+    <Combobox.Root
+      items={users}
+      value={value}
+      onValueChange={setValue}
+      isItemEqualToValue={(item, selected) => item.value === selected.value}
+      itemToStringLabel={(item) => item.label}
+    >
       <Combobox.Trigger>
         <Combobox.Value placeholder="Select a user" />
       </Combobox.Trigger>
       <Combobox.Content>
         <Combobox.Search placeholder="Search users..." />
         <Combobox.List>
-          {users.map((user) => (
+          {(user: User) => (
             <Combobox.Item key={user.value} value={user}>
               {user.label}
             </Combobox.Item>
-          ))}
+          )}
         </Combobox.List>
       </Combobox.Content>
     </Combobox.Root>
